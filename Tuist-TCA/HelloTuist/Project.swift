0a78e9e69b093ac0f7b3ed8747d6d24e1744e629
kdy <kokbael@naver.com>
2025-04-21 11:27:49 +0900

    프로젝트를 모듈 단위로 분리: ProductFeature, Network 모듈 추가 Modules 폴더 추가(Modules/Network/Sources, Modules/Network/Tests, Modules/ProductFeature/Sources)

--- Tuist-TCA/HelloTuist/Project.swift
+++ Tuist-TCA/HelloTuist/Project.swift
@@ -1,10 +1,12 @@
 import ProjectDescription
 
+let bundleId = "kr.co.codegrove.HelloTuist"
+
 let appTarget = Target.target(
     name: "HelloTuist",
     destinations: .iOS,
     product: .app,
-    bundleId: "io.tuist.HelloTuist",
+    bundleId: bundleId,
     infoPlist: .extendingDefault(
         with: [
             "UILaunchScreen": [
@@ -16,7 +18,7 @@ let appTarget = Target.target(
     sources: ["HelloTuist/Sources/**"],
     resources: ["HelloTuist/Resources/**"],
     dependencies: [
-        .package(product: "Alamofire"),
+        .target(name: "ProductFeature")
     ]
 )
 
@@ -31,11 +33,52 @@ let testTarget = Target.target(
     dependencies: [.target(name: "HelloTuist")]
 )
 
+// MARK: - Modules
+let productFeatureModule = Target.target(
+    name: "ProductFeature",
+    destinations: .iOS,
+    product: .framework, // App에서 사용할 것이므로 framework
+    bundleId: "\(bundleId).ProductFeature",
+    infoPlist: .default,
+    sources: ["Modules/ProductFeature/Sources/**"],
+    dependencies: [
+        .target(name: "Network") // Network 모듈의 APIService, Product 모델 사용
+    ]
+)
+
+
+let networkModule = Target.target(
+    name: "Network",
+    destinations: .iOS,
+    product: .framework, // 다른 모듈에서 사용할 것이므로 framework로 설정
+    bundleId: "\(bundleId).Network",
+    infoPlist: .default,
+    sources: ["Modules/Network/Sources/**"],
+    dependencies: [
+        .package(product: "Alamofire"),
+    ]
+)
+
+let networkTests = Target.target(
+    name: "NetworkTests",
+    destinations: .iOS,
+    product: .unitTests,
+    bundleId: "\(bundleId).NetworkTests",
+    infoPlist: .default,
+    sources: ["Modules/Network/Tests/**"],
+    dependencies: [
+        .target(name: "Network") // Network 모듈 테스트
+    ]
+)
+
 
 let project = Project(
     name: "HelloTuist",
     targets: [
         appTarget,
-        testTarget
+        testTarget,
+        productFeatureModule,
+        networkModule,
+        networkTests
     ]
 )
