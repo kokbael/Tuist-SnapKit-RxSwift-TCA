62f0b5cd8c6adc0152e90f0927a73daade38a183
kdy <kokbael@naver.com>
2025-04-22 09:49:00 +0900

    AppDelegate 및 ViewController 추가: SwiftUI 구조에서 UIKit으로 전환

---
+++ Snapkit/SnapkitDemo/SnapkitDemo/Sources/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  SnapKitDemo
+//
+//  Created by 김동영 on 4/22/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "SnapKit Demo"
+        view.backgroundColor = .white
+    }
+    
+}
