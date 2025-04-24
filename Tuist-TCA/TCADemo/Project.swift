f27f89bb763493092f89fccfd8b78382b7c1a5c7
kdy <kokbael@naver.com>
2025-04-24 10:09:14 +0900

    TCADemo 프로젝트 생성

---
+++ Tuist-TCA/TCADemo/Project.swift
@@ -0,0 +1,36 @@
+import ProjectDescription
+
+let project = Project(
+    name: "TCADemo",
+    targets: [
+        .target(
+            name: "TCADemo",
+            destinations: .iOS,
+            product: .app,
+            bundleId: "kr.co.codegrove.TCADemo",
+            infoPlist: .extendingDefault(
+                with: [
+                    "UILaunchScreen": [
+                        "UIColorName": "",
+                        "UIImageName": "",
+                    ],
+                ]
+            ),
+            sources: ["TCADemo/Sources/**"],
+            resources: ["TCADemo/Resources/**"],
+            dependencies: [
+                .external(name: "ComposableArchitecture")
+            ]
+        ),
+        .target(
+            name: "TCADemoTests",
+            destinations: .iOS,
+            product: .unitTests,
+            bundleId: "kr.co.codegrove.TCADemoTests",
+            infoPlist: .default,
+            sources: ["TCADemo/Tests/**"],
+            resources: [],
+            dependencies: [.target(name: "TCADemo")]
+        ),
+    ]
+)
