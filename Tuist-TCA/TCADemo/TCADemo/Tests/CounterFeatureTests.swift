63c15c7b697e3b1a1da597033e5c4d8bf6b8a850
kdy <kokbael@naver.com>
2025-04-24 14:53:20 +0900

    테스트 리팩토링: Increment 및 Decrement 버튼 테스트를 개별 함수로 분리하고 초기 상태 수정

--- Tuist-TCA/TCADemo/TCADemo/Tests/CounterFeatureTests.swift
+++ Tuist-TCA/TCADemo/TCADemo/Tests/CounterFeatureTests.swift
@@ -5,16 +5,29 @@ import Testing
 
 @MainActor
 struct CounterFeatureTests {
-    @Test
-    func basics() async {
-        let store = TestStore(initialState: CounterFeature.State()) {
+    // Arrange
+    let store: TestStore<CounterFeature.State, CounterFeature.Action>
+    
+    init() throws {
+        store = TestStore(initialState: CounterFeature.State()) {
             CounterFeature()
         }
-        
+    }
+    
+    @Test
+    func testIncrementButtonWorks() async {
+        // Act
         await store.send(.incrementButtonTapped) { state in
+            // Assert
             state.count = 1
         }
+    }
+    
+    @Test
+    func testDecrementButtonWorks() async {
+        // Act
         await store.send(.decrementButtonTapped) {
+            // Assert
             $0.count = 0
         }
     }
